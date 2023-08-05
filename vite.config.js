a2e1e519be8c5a3298ec20dcbf21dd8cb9a93b77
Sachin <sachinsen249@gmail.com>
2023-08-06 03:42:27 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,25 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-
+// https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
 // })
 
-import macrosPlugin from "vite-plugin-babel-macros"
-
-export default {
-	// ...
-	plugins: [
-		// ...
-		macrosPlugin(),
-	],
-}
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            {
+              displayName: true,
+              fileName: false
+            }
+          ]
+        ]
+      }
+    })
+  ]
+})
